351b582393008498cd8ba1c0dbcbdc39c23ee2ef
Mark Gibson <gibsonma@tcd.ie>
2013-11-06 16:11:46 +0000

    Forgot to add files for last commit

--- Data Structures/Single_Producer_Single_Consumer_Queue/spsc.c
+++ Data Structures/Single_Producer_Single_Consumer_Queue/spsc.c
@@ -4,24 +4,28 @@
 #include <time.h>
 #include <sys/time.h>
 #define TIMING
-#define LOCKED
-//#define LOCKLESSS
+#define EBUSY 1
+//#define LOCKED
+#define LOCKLESS
 
-
+void acquire(int id);
+void release(int id);
 void *Producer();
 void *Consumer();
 
 #ifdef TIMING
-	struct timeval start_time;
-	struct timeval stop_time;
-	long long total_time = 0;
+struct timeval start_time;
+struct timeval stop_time;
+long long total_time = 0;
 #endif
 
 const int BUFFER_SIZE = 10;//Maximum size of buffer
 const int BUFFER_MIN = 0;//Minimim size of buffer
 const int NUM_THREADS = 2;//Number of threads used
-const long long MAX_ITERATIONS = 1000;//Maximum number of iterations for the program
+const long long MAX_ITERATIONS = 1000000;//Maximum number of iterations for the program
 
+int flag[2];
+int last;
 int bufferIndex = 0;//Tracks number of items in buffer
 long long iterations = 0;//Tracks number of iterations completed
 char *BUFFER;//Buffer represented by array of chars
@@ -63,16 +67,24 @@ void *Producer(void * threadid)
 {    
 	while(iterations < MAX_ITERATIONS)
 	{
+#ifdef LOCKED		
 		pthread_mutex_lock(&bufferLock);//Lock shared variable
 		if(bufferIndex == BUFFER_SIZE)//If buffer is full then wait
 		{                        
 			pthread_cond_wait(&bufferNotFull,&bufferLock);
 		}                        
-		printf("Produce : %d \n", bufferIndex);
 		iterations++;
 		BUFFER[bufferIndex++] = '@';
 		pthread_mutex_unlock(&bufferLock);//Unlock shared variable
 		pthread_cond_signal(&bufferNotEmpty);//Signal that buffer is not empty
+#endif	
+#ifdef LOCKLESS
+		while(bufferIndex == BUFFER_SIZE);
+		acquire(0);
+		iterations++;
+		BUFFER[bufferIndex++] = '@';
+		release(0);
+#endif
 	}    
 }
 
@@ -80,15 +92,37 @@ void *Consumer()
 {
 	while(iterations < MAX_ITERATIONS)
 	{
+#ifdef LOCKED		
 		pthread_mutex_lock(&bufferLock);
 		if(bufferIndex == 0)//If buffer is empty then wait
 		{            
 			pthread_cond_wait(&bufferNotEmpty,&bufferLock);
 		}                
-		printf("Consume : %d \n", bufferIndex);
 		iterations++;
 		bufferIndex--;
 		pthread_mutex_unlock(&bufferLock);        
 		pthread_cond_signal(&bufferNotFull);//Signal that buffer is not full  
+#endif
+#ifdef LOCKLESS
+		while(bufferIndex == BUFFER_MIN);
+		acquire(1);
+		iterations++;
+		bufferIndex--;
+		release(1);
+#endif
+
 	}    
 }
+
+void acquire(int id) // id is the thread ID [0 or 1] 
+{ 
+	int j = 1 - id; // 0 -> 1 and 1 ->0 
+	flag[id] = 1; // want lock 
+	last = id; // other thread has priority 
+	while (flag[j] && last == id); 
+} 
+
+void release(int id) 
+{ 
+	flag[id] = 0; // release lock 
+} 
