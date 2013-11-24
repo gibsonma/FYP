750fafe21e49cc3866e0efc01d401b3e0643ce4a
Mark Gibson <gibsonma@tcd.ie>
2013-11-24 14:30:19 +0000

    Tidied up the ring buffer code. I also collected some data from stoker and put it into excel with both locked and lockless modes. Next step is to collect data on my machine and add this to excel.

--- Data Structures/Ring_Buffer/ring.c
+++ Data Structures/Ring_Buffer/ring.c
@@ -2,16 +2,15 @@
  * Spin lock code taken from http://locklessinc.com
  * Circular buffer example, keeps one slot open 
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/time.h>
 #define TIMING
-//#define LOCKED
-#define LOCKLESS
+#define LOCKED
+//#define LOCKLESS
+//#define DEBUG
 #define barrier() asm volatile("": : :"memory")
-#define cpu_relax() asm volatile("pause\n": : :"memory")
 #define EBUSY 1
 
 #ifdef LOCKLESS
@@ -21,18 +20,15 @@ static inline unsigned xchg_32(void *ptr, unsigned x)
 			:"=r" ((unsigned) x)
 			:"m" (*(volatile unsigned *)ptr), "0" (x)
 			:"memory");
-
 	return x;
 }
 typedef unsigned spinlock;
-spinlock spinLock;
+spinlock spinLockWrite, spinLockRead;
 static void spin_lock(spinlock *lock)
 {
 	while (1)
 	{
 		if (!xchg_32(lock, EBUSY)) return;
-
-		while (*lock) cpu_relax();
 	}
 }
 
@@ -50,9 +46,9 @@ long long tstart = 0,  tstop = 0;
 #endif
 
 pthread_mutex_t bufferLock = PTHREAD_MUTEX_INITIALIZER;
-const int NUM_THREADS = 8;
-const int sizeOfBuffer = 10;
-const int EXECUTION_TIME = 1;
+int NUM_THREADS = 2;
+const int sizeOfBuffer = 1000;
+const int EXECUTION_TIME = 1;//Determines how long the program runs for in seconds
 volatile long long iterations = 0;
 
 /* Opaque buffer element type.  This would be defined by the application. */
@@ -96,28 +92,26 @@ void *cbWrite(void * threadid) {
 	{
 #ifdef LOCKED		
 		pthread_mutex_lock(&bufferLock);
+#endif
+#ifdef LOCKLESS
+		spin_lock(&spinLockWrite);
+#endif
+#ifdef DEBUG		
 		printf("Value: %d placed in index: %d\n", count.value + 1, cb.end);
+#endif
 		count.value++;
 		cb.elems[cb.end] = count;
 		cb.end = (cb.end + 1) % cb.size;
 		if (cb.end == cb.start)
 			cb.start = (cb.start + 1) % cb.size; /* full, overwrite */
 		iterations++;
+#ifdef LOCKED		
 		pthread_mutex_unlock(&bufferLock);
 #endif	
-
 #ifdef LOCKLESS
-		spin_lock(&spinLock);
-		printf("Value: %d placed in index: %d\n", count.value + 1, cb.end);
-		count.value++;
-		cb.elems[cb.end] = count;
-		cb.end = (cb.end + 1) % cb.size;
-		if (cb.end == cb.start)
-			cb.start = (cb.start + 1) % cb.size; /* full, overwrite */
-		iterations++;
-		spin_unlock(&spinLock);
+		spin_unlock(&spinLockWrite);
 #endif
-		gettimeofday(&stop_time, NULL);
+		gettimeofday(&stop_time, NULL);//If the thread has run for one second or more then stop
 		if(((stop_time.tv_sec + (stop_time.tv_usec/1000000.0)) -( start_time.tv_sec + (start_time.tv_usec/1000000.0))) > EXECUTION_TIME) break;
 	}
 }
@@ -128,18 +122,20 @@ void *cbRead(void * threadid) {
 	{
 #ifdef LOCKED
 		pthread_mutex_lock(&bufferLock);
+#endif
+#ifdef LOCKLESS
+		spin_lock(&spinLockRead);
+#endif 
 		tmp = cb.elems[cb.start];
+#ifdef DEBUG		
 		printf("Value: %d read from index: %d\n", tmp.value, cb.start);
+#endif		
 		cb.start = (cb.start + 1) % cb.size;
+#ifdef LOCKED		
 		pthread_mutex_unlock(&bufferLock);
 #endif
-
 #ifdef LOCKLESS
-		spin_lock(&spinLock);
-		tmp = cb.elems[cb.start];
-		printf("Value: %d read from index: %d\n", tmp.value, cb.start);
-		cb.start = (cb.start + 1) % cb.size;
-		spin_unlock(&spinLock);
+		spin_unlock(&spinLockRead);
 #endif
 		gettimeofday(&stop_time, NULL);
 		if(((stop_time.tv_sec + (stop_time.tv_usec/1000000.0)) -( start_time.tv_sec + (start_time.tv_usec/1000000.0))) > EXECUTION_TIME) break;
@@ -149,14 +145,18 @@ void *cbRead(void * threadid) {
 int main(int argc, char **argv) 
 {
 #ifdef TIMING
-	gettimeofday(&start_time, NULL);
+//	gettimeofday(&start_time, NULL);
 #endif
+//	cbInit(&cb, sizeOfBuffer);
+//	int t, rc;
+	for(NUM_THREADS = 1; NUM_THREADS < 256; NUM_THREADS = NUM_THREADS * 2)
+	{
+	gettimeofday(&start_time, NULL);
 	cbInit(&cb, sizeOfBuffer);
 	int t, rc;
 	pthread_t threads[NUM_THREADS];
 	for(t = 0; t < NUM_THREADS; t++)
 	{
-		printf("Creating Thread %d\n", t);
 		if(t % 2 == 0)rc = pthread_create(&threads[t], NULL, cbWrite, (void *) t);
 		else rc = pthread_create(&threads[t], NULL, cbRead, (void *) t);
 	}
@@ -164,11 +164,13 @@ int main(int argc, char **argv)
 	{
 		pthread_join(threads[t], NULL);
 	}
-	cbFree(&cb);
+	//cbFree(&cb);
 #ifdef TIMING
 	gettimeofday(&stop_time, NULL);
 	total_time += (stop_time.tv_sec - start_time.tv_sec) * 1000000L + (stop_time.tv_usec - start_time.tv_usec);
-	printf("Total executing time %lld microseconds with %lld iterations per second\n", total_time, iterations/EXECUTION_TIME);
+	printf("Total executing time %lld microseconds with %lld iterations per second and %d threads\n", total_time, iterations/EXECUTION_TIME, NUM_THREADS);
 #endif
+	}
+	cbFree(&cb);
 	return 0;
 }
