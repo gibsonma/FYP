ef8a7e6d09b78159c879d1577dfcc064630b9e41
Mark Gibson <gibsonma@tcd.ie>
2014-01-28 16:21:41 +0000

    Gathered data from local machine, ducss and netsoc. VPN only has one core so I cannot use it. I tested the assembly spinlock against the c++ variant, minimal difference, hence I removed the assembly code from ring.cpp. Started adding to my report in a meaningful fashion.

--- Data Structures/Ring_Buffer/ring.cpp
+++ Data Structures/Ring_Buffer/ring.cpp
@@ -15,10 +15,6 @@
 #define EXECUTION_TIME 1
 #define PAUSE 2
 
-//If windows
-//#include <atomic>
-//#include <unistd.h>
-
 //Modes of Operation - If neither defined then defaults to assembly spinlock
 #define LOCKED //Uses simple mutex blocking
 //#define SPINLOCK //Uses a spinlock implemented with C++ atomics
@@ -35,41 +31,6 @@ int back, front, count = 0; //Index of oldest element / Index to write new eleme
 int *buffer; //Buffer of elements
 std::atomic<int> lock (0);
 
-/* Implementation of a spinlock in assembly */
-#define EBUSY 1
-#define cpu_relax() asm volatile("pause\n": : :"memory")
-#define barrier() asm volatile("": : :"memory")
-static inline unsigned xchg_32(void *ptr, unsigned x)
-{
-	__asm__ __volatile__("xchgl %0,%1"
-			:"=r" ((unsigned) x)
-			:"m" (*(volatile unsigned *)ptr), "0" (x)
-			:"memory");
-
-	return x;
-}
-typedef unsigned spinlock;
-spinlock spinLock;
-static void spin_lock(spinlock *lock)
-{
-	while (1)
-	{
-		if (!xchg_32(lock, EBUSY)) return;
-
-		while (*lock) cpu_relax();
-	}
-}
-
-static void spin_unlock(spinlock *lock)
-{
-	barrier();
-	*lock = 0;
-}
-
-static int spin_trylock(spinlock *lock)
-{
-	return xchg_32(lock, EBUSY);
-}
 void* push(void* threadid)
 {
 	while(1)
@@ -80,9 +41,6 @@ void* push(void* threadid)
 		do{					//C++ spinlock
 			while(lock.load() == 1)usleep(PAUSE);
 		}while(lock.exchange(1));
-#else		
-		//		while(spin_trylock(&spinLock) == 0);
-		spin_lock(&spinLock);			//Assembly spinlock
 #endif
 		int item = rand() % 100;
 		iterations++;
@@ -108,8 +66,6 @@ void* push(void* threadid)
 		pthread_mutex_unlock(&bufferLock);
 #elif defined(SPINLOCK)
 		lock = 0;
-#else
-		spin_unlock(&spinLock);
 #endif	
 		gettimeofday(&stop_time, NULL);//If the thread has run for one second or more then stop
 		if(((stop_time.tv_sec + (stop_time.tv_usec/1000000.0)) -( start_time.tv_sec + (start_time.tv_usec/1000000.0))) > EXECUTION_TIME) break;
@@ -126,9 +82,6 @@ void* pop(void* threadid)
 		do{
 			while(lock.load() == 1)usleep(PAUSE);
 		}while(lock.exchange(1));
-#else
-		//		while(spin_trylock(&spinLock) == 0);
-		spin_lock(&spinLock);
 #endif
 		iterations++;
 		if (front == -1 && back == -1) {
@@ -155,8 +108,6 @@ void* pop(void* threadid)
 		pthread_mutex_unlock(&bufferLock);
 #elif defined(SPINLOCK)
 		lock = 0;
-#else		
-		spin_unlock(&spinLock);
 #endif
 		gettimeofday(&stop_time, NULL);//If the thread has run for one second or more then stop
 		if(((stop_time.tv_sec + (stop_time.tv_usec/1000000.0)) -( start_time.tv_sec + (start_time.tv_usec/1000000.0))) > EXECUTION_TIME) break;
@@ -175,25 +126,25 @@ void printBuffer()
 int main()
 {
 	for(int i = 1; i <= MAX_THREAD_VAL; i = i * 2){
-	srand(time(NULL));
-	gettimeofday(&start_time, NULL);
-	int rc, t;
-	buffer = new int[SIZE];
-	pthread_t threads[i];
-	for(t = 0; t < i; t++)
-	{
-		if(t % 2 == 0)rc = pthread_create(&threads[t], NULL, push, (void *)t);
-		else rc = pthread_create(&threads[t], NULL, pop, (void *)t);
-	}
-	for(t = 0; t < i; t++)
-	{
-		pthread_join(threads[t], NULL);
-	}
-	gettimeofday(&stop_time, NULL);
-	total_time += (stop_time.tv_sec - start_time.tv_sec) * 1000000L + (stop_time.tv_usec - start_time.tv_usec);
-	printf("%lld ,",iterations/EXECUTION_TIME);
-	//printf("Total executing time %lld microseconds with %lld iterations per second and %d threads\n", total_time, iterations/EXECUTION_TIME, i);
-	iterations = 0;
+		srand(time(NULL));
+		gettimeofday(&start_time, NULL);
+		int rc, t;
+		buffer = new int[SIZE];
+		pthread_t threads[i];
+		for(t = 0; t < i; t++)
+		{
+			if(t % 2 == 0)rc = pthread_create(&threads[t], NULL, push, (void *)t);
+			else rc = pthread_create(&threads[t], NULL, pop, (void *)t);
+		}
+		for(t = 0; t < i; t++)
+		{
+			pthread_join(threads[t], NULL);
+		}
+		gettimeofday(&stop_time, NULL);
+		total_time += (stop_time.tv_sec - start_time.tv_sec) * 1000000L + (stop_time.tv_usec - start_time.tv_usec);
+		printf("%lld ,",iterations/EXECUTION_TIME);
+		//printf("Total executing time %lld microseconds with %lld iterations per second and %d threads\n", total_time, iterations/EXECUTION_TIME, i);
+		iterations = 0;
 	}
 
 	return 0;
