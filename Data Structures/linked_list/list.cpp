2a2e7cf234a6634fe4b9357f2ffda65a83fa0de9
Mark Gibson <gibsonma@tcd.ie>
2014-02-18 17:11:19 +0000

    Finished locked linked list, add modes of operation and gathered data from stoker, local and cube. Added CASLOCKND which is a CAS lock but with no delay. In addition, I ran tests to determine if locked performance differed with different key ranges. In most cases it does not, though with a very small key range such as 10, progress slows drastically.

--- Data Structures/linked_list/list.cpp
+++ Data Structures/linked_list/list.cpp
@@ -10,19 +10,22 @@
 #include <atomic>
 #include <unistd.h>
 #define NUM_THREADS 1
-#define MAX_THREAD_VAL 8
-#define KEY_RANGE 5
+#define MAX_THREAD_VAL 128
+#define KEY_RANGE 10000
 #define EXECUTION_TIME 1
 #define PAUSE 2
 #define MIN_DELAY 1
 #define MAX_DELAY 10
-
+//#define DEBUG
 //Modes of Operation - If neither defined then defaults to assembly spinlock
 //#define LOCKED //Uses simple mutex blocking
-//#define SPINLOCK //Uses a spinlock implemented with C++ atomics
+//#define TTAS //Uses a spinlock implemented with C++ atomics
+//#define TTASNP
 //#define CASLOCK
+//#define CASLOCKND
 //#define TAS
-//#define TICKET
+//#define TASWP
+#define TICKET
 
 pthread_mutex_t listLock = PTHREAD_MUTEX_INITIALIZER;
 
@@ -39,9 +42,9 @@ std::atomic<long> ticket (0);//For ticket lock
 void printList();
 class Node{
 	public:
-	volatile int key;
-	Node * volatile next;
-	Node(int, Node *);
+		volatile int key;
+		Node * volatile next;
+		Node(int, Node *);
 };
 Node::Node(int val, Node * ptr)
 {
@@ -52,84 +55,164 @@ Node * volatile head = NULL;
 
 void* add(void* threadid)
 {
-	
-	pthread_mutex_lock(&listLock);
-	int key = rand() % KEY_RANGE;
-	cout << "Attempting to add " << key << "\n";
-	Node * volatile tmpHead = head;
-	Node * newNode = new Node(key, NULL);
-	Node * volatile tmpNode = head;
-	Node * volatile prevNode;
-	if(tmpNode == NULL)//If linked list is empty
+
+	while(true)
 	{
-		cout << "Linked list is empty \n";
-		head = newNode;//Set head to be new node
-		
-	}
-	else if(tmpNode->key > newNode->key)//If new key is smaller than head
-	{
-		cout << "New node is smaller than head \n";
-		newNode->next = tmpNode;
-		head = newNode;
-		
-	}
-	else//Traverse list until entry point found
-	{
-		while(tmpNode && (tmpNode->key < newNode->key))
-		{
-			prevNode = tmpNode;
-			tmpNode = tmpNode->next;
+#if defined(LOCKED)
+		pthread_mutex_lock(&listLock);
+#elif defined(TTAS)
+		do{
+			while(lock.load() == 1)sleep(PAUSE);
+		}while(lock.exchange(1));
+#elif defined(TTASNP)
+		do{
+			while(lock.load() == 1);
+		}while(lock.exchange(1));
+#elif defined(CASLOCK)
+		int delay = MIN_DELAY;
+		while(true){
+			if(lock.compare_exchange_strong(notTaken, taken))break;
+			sleep(rand() % delay);
+			if(delay < MAX_DELAY)delay = 2 * delay;
 		}
-		if(tmpNode && (tmpNode->key == newNode->key))
-		{
-			cout << "Node already present\n";
+#elif defined(CASLOCKND)
+		while(true){
+			if(lock.compare_exchange_strong(notTaken, taken))break;
 		}
-		else
+#elif defined(TAS)
+		while(lock.exchange(1));
+#elif defined(TASWP)
+		while(lock.exchange(1))sleep(PAUSE);
+#elif defined(TICKET)
+		int myTicket = ticket.fetch_add(1);
+		while(myTicket != nowServing)sleep(myTicket - nowServing);
+#endif
+		int key = rand() % KEY_RANGE;
+		iterations++;
+		//cout << "Attempting to add " << key << "\n";
+		Node * volatile tmpHead = head;
+		Node * newNode = new Node(key, NULL);
+		Node * volatile tmpNode = head;
+		Node * volatile prevNode;
+		if(tmpNode == NULL)//If linked list is empty
 		{
-			cout << "Adding Node\n";
+			//	cout << "Linked list is empty \n";
+			head = newNode;//Set head to be new node
+
+		}
+		else if(tmpNode->key > newNode->key)//If new key is smaller than head
+		{
+			//	cout << "New node is smaller than head \n";
 			newNode->next = tmpNode;
-			prevNode->next = newNode;
+			head = newNode;
+
 		}
-		
+		else//Traverse list until entry point found
+		{
+			while(tmpNode && (tmpNode->key < newNode->key))
+			{
+				prevNode = tmpNode;
+				tmpNode = tmpNode->next;
+			}
+			if(tmpNode && (tmpNode->key == newNode->key))
+			{
+				//		cout << "Node already present\n";
+			}
+			else
+			{
+				//		cout << "Adding Node\n";
+				newNode->next = tmpNode;
+				prevNode->next = newNode;
+			}
+
+		}
+		//	printList();
+#if defined(LOCKED)
+		pthread_mutex_unlock(&listLock);
+#elif defined(TICKET)
+		nowServing++;
+#else
+		lock = 0;
+#endif
+		gettimeofday(&stop_time, NULL);
+		if(((stop_time.tv_sec + (stop_time.tv_usec/1000000.0)) -( start_time.tv_sec + (start_time.tv_usec/1000000.0))) > EXECUTION_TIME) break;
 	}
-	printList();
-	pthread_mutex_unlock(&listLock);
-	
+
 }
 
 void* remove(void* threadid)
 {
-	pthread_mutex_lock(&listLock);
-	int key = rand() % KEY_RANGE;
-	cout << "Attempting to remove " << key << "\n";
-	Node * tmpNode = head;
-	Node * prevNode;
-	if(tmpNode == NULL)//If linked list is empty then return
+	while(true)
 	{
-		cout << "List is empty\n";
-		
-	}
-	else if(tmpNode->key == key)//Else if node is head then reassign head
-	{
-		head = tmpNode->next;
-		delete tmpNode;
-	}
-	else//Else traverse list
-	{
-		while(tmpNode && (tmpNode->key != key))
-		{
-			prevNode = tmpNode;
-			tmpNode = tmpNode->next;
+#if defined(LOCKED)
+		pthread_mutex_lock(&listLock);
+#elif defined(TTAS)
+		do{
+			while(lock.load() == 1)sleep(PAUSE);
+		}while(lock.exchange(1));
+#elif defined(TTASNP)
+		do{
+			while(lock.load() == 1);
+		}while(lock.exchange(1));
+#elif defined(CASLOCK)
+		int delay = MIN_DELAY;
+		while(true){
+			if(lock.compare_exchange_strong(notTaken, taken))break;
+			sleep(rand() % delay);
+			if(delay < MAX_DELAY)delay = 2 * delay;
 		}
-		if(tmpNode && (tmpNode->key == key))
-		{
-			cout << "Removing node" << key << "\n";
-			prevNode->next = tmpNode->next;
+#elif defined(CASLOCKND)
+		while(true){
+			if(lock.compare_exchange_strong(notTaken, taken))break;
 		}
-		else cout <<"Node does not exist\n";
+#elif defined(TAS)
+		while(lock.exchange(1));
+#elif defined(TASWP)
+		while(lock.exchange(1))sleep(PAUSE);
+#elif defined(TICKET)
+		int myTicket = ticket.fetch_add(1);
+		while(myTicket != nowServing)sleep(myTicket - nowServing);
+#endif
+		int key = rand() % KEY_RANGE;
+		iterations++;
+		//cout << "Attempting to remove " << key << "\n";
+		Node * tmpNode = head;
+		Node * prevNode;
+		if(tmpNode == NULL)//If linked list is empty then return
+		{
+			//	cout << "List is empty\n";
+
+		}
+		else if(tmpNode->key == key)//Else if node is head then reassign head
+		{
+			head = tmpNode->next;
+			delete tmpNode;
+		}
+		else//Else traverse list
+		{
+			while(tmpNode && (tmpNode->key != key))
+			{
+				prevNode = tmpNode;
+				tmpNode = tmpNode->next;
+			}
+			if(tmpNode && (tmpNode->key == key))
+			{
+				//		cout << "Removing node" << key << "\n";
+				prevNode->next = tmpNode->next;
+			}
+			//	else cout <<"Node does not exist\n";
+		}
+		//	printList();
+#if defined(LOCKED)
+		pthread_mutex_unlock(&listLock);
+#elif defined(TICKET)
+		nowServing++;
+#else
+		lock = 0;
+#endif
+		gettimeofday(&stop_time, NULL);
+		if(((stop_time.tv_sec + (stop_time.tv_usec/1000000.0)) -( start_time.tv_sec + (start_time.tv_usec/1000000.0))) > EXECUTION_TIME) break;
 	}
-	printList();
-	pthread_mutex_unlock(&listLock);
 }
 
 
@@ -161,13 +244,14 @@ int main()
 		{
 			pthread_join(threads[t], NULL);
 		}
-	//	gettimeofday(&stop_time, NULL);
-	//	total_time += (stop_time.tv_sec - start_time.tv_sec) * 1000000L + (stop_time.tv_usec - start_time.tv_usec);
-	//	printf("%lld ,",iterations/EXECUTION_TIME);
-//		printf("Total executing time %lld microseconds with %lld iterations per second and %d threads\n", total_time, iterations/EXECUTION_TIME, i);
+		gettimeofday(&stop_time, NULL);
+		total_time += (stop_time.tv_sec - start_time.tv_sec) * 1000000L + (stop_time.tv_usec - start_time.tv_usec);
+		printf("%lld ,",iterations/EXECUTION_TIME);
+	//	printf("Total executing time %lld microseconds with %lld iterations per second and %d threads\n", total_time, iterations/EXECUTION_TIME, i);
 		iterations = 0;
 	}
+#if defined(DEBUG)
 	cout << "Final list: "; printList();
-
+#endif
 	return 0;
 }
